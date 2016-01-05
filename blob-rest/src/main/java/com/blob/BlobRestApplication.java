5fde684305d0f78152b392a5fd27d6744530e638
rajupbiz <rajupatel.biz@gmail.com>
2016-01-05 19:29:07 +0530

    init

--- blob-rest/src/main/java/com/blob/BlobRestApplication.java
+++ blob-rest/src/main/java/com/blob/BlobRestApplication.java
@@ -8,5 +8,8 @@ public class BlobRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlobRestApplication.class, args);
+		
+		System.out.println("    BlobRestApplication  ....  ");
+		
 	}
 }
--- blob-rest/src/main/java/com/blob/restctrl/TestCtrl.java
+++ blob-rest/src/main/java/com/blob/restctrl/TestCtrl.java
@@ -1,6 +1,7 @@
 package com.blob.restctrl;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +13,31 @@ import com.blob.model.Contact;
 @RestController
 public class TestCtrl {
 
-	@RequestMapping(value="/contact/{id}", method=RequestMethod.GET)
+	@RequestMapping(value="/contact/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Contact> getContact(@PathVariable Long id){
 		
 		System.out.println("  id ===>   "+id);
 		
 		Contact c = new Contact();
 		c.setId(id);
-		c.setName("Max");
+		c.setFirstName("Max");
 		
 		return new ResponseEntity<Contact>(c, HttpStatus.OK);
 	}
-
+	
+	@RequestMapping(value="/contact", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Contact> getAllContacts(){
+		
+		System.out.println("  getAllContacts ===>   ");
+		
+		Contact c = new Contact();
+		c.setId(1L);
+		c.setFirstName("Max");
+		
+		c = new Contact();
+		c.setId(2L);
+		c.setFirstName("Mark");
+		
+		return new ResponseEntity<Contact>(c, HttpStatus.OK);
+	}
 }
