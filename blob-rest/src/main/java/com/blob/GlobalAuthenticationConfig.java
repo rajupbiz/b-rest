b3e58279348b9caa16d7206054a1080e3bd243f9
rajupbiz <rajupatel.biz@gmail.com>
2016-01-06 21:06:31 +0530

    init

---
+++ blob-rest/src/main/java/com/blob/GlobalAuthenticationConfig.java
@@ -0,0 +1,20 @@
+package com.blob;
+
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class GlobalAuthenticationConfig extends GlobalAuthenticationConfigurerAdapter {
+
+	@Resource
+	private UserDetailsService userDetailService;
+	
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailService);
+	}
+}
--- blob-rest/src/main/java/com/blob/restctrl/TestCtrl.java
+++ blob-rest/src/main/java/com/blob/restctrl/TestCtrl.java
@@ -1,34 +1,54 @@
 package com.blob.restctrl;
 
-import org.springframework.http.HttpStatus;
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.blob.dao.ContactDao;
 import com.blob.model.Contact;
 
 @RestController
+@RequestMapping(value="/contact")
 public class TestCtrl {
+	
+	@Resource
+	private ContactDao contactDao;
 
-	@RequestMapping(value="/contact/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Contact> getContact(@PathVariable Long id){
-		
+	@RequestMapping(value="/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Contact getContact(@PathVariable Long id){
 		System.out.println("  id ===>   "+id);
-		
-		Contact c = new Contact();
-		c.setId(id);
-		c.setFirstName("Max");
-		
-		return new ResponseEntity<Contact>(c, HttpStatus.OK);
+		Contact contact = contactDao.findOne(id);
+		return contact;
 	}
 	
-	@RequestMapping(value="/contact", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Contact> getAllContacts(){
-		
+	@RequestMapping(value="", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Contact> getAllContacts(){
 		System.out.println("  getAllContacts ===>   ");
+		List<Contact> contacts = contactDao.findAll();
+		return contacts;
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Long createContact(Contact c){
+		System.out.println("  create contact "+c.getFirstName());
+		
+		if(c != null && c.getFirstName() != null){
+			c = contactDao.save(c);
+		}
+		return c.getId();
+	}
+	
+	/*@RequestMapping(value="/", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Contact> hello(){
+		
+		System.out.println("  hello ===>   ");
 		
 		Contact c = new Contact();
 		c.setId(1L);
@@ -39,5 +59,5 @@ public class TestCtrl {
 		c.setFirstName("Mark");
 		
 		return new ResponseEntity<Contact>(c, HttpStatus.OK);
-	}
+	}*/
 }
