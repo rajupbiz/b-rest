238c97ba471fbc20ac4bd26efc021d8fc7ebc511
rajupbiz <rajupatel.biz@gmail.com>
2016-01-30 23:59:34 +0530

    init

--- blob-rest/src/main/java/com/blob/CwpApplication.java
+++ blob-rest/src/main/java/com/blob/CwpApplication.java
@@ -1,13 +1,14 @@
 package com.blob;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlobRestApplication {
+public class CwpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlobRestApplication.class, args);
+		SpringApplication.run(CwpApplication.class, args);
 		
 		System.out.println("    BlobRestApplication  ....  ");
 		
--- blob-rest/src/main/java/com/blob/web/rest/TestCtrl.java
+++ blob-rest/src/main/java/com/blob/web/rest/TestCtrl.java
@@ -1,4 +1,4 @@
-package com.blob.restctrl;
+package com.blob.web.rest;
 
 import java.util.List;
 
